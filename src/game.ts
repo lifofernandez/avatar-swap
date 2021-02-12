f356e0d35dc093b4c66649fd81e8800f7fb58461
nearnshaw <nearnshaw@decentraland.org>
2021-02-12 12:15:29 -0300

    new utils lib version

--- src/game.ts
+++ src/game.ts
@@ -1,28 +1,46 @@
-import utils from "../node_modules/decentraland-ecs-utils/index"
-import { Arissa } from "./arissa"
+import * as utils from '@dcl/ecs-scene-utils'
+import { Arissa } from './arissa'
 
 // Base
 const base = new Entity()
-base.addComponent(new GLTFShape("models/baseGrass.glb"))
+base.addComponent(new GLTFShape('models/baseGrass.glb'))
 engine.addEntity(base)
 
 // Arissa
-const arissa = new Arissa(new GLTFShape("models/arissa.glb"), new Transform({ position: new Vector3(0, 0.05, -0.10), scale: new Vector3(0, 0, 0)}))
+const arissa = new Arissa(
+  new GLTFShape('models/arissa.glb'),
+  new Transform({
+    position: new Vector3(0, 0.05, -0.1),
+    scale: new Vector3(0, 0, 0),
+  })
+)
 arissa.setParent(Attachable.AVATAR)
 
 // Hide avatars
 const hideAvatarsEntity = new Entity()
-hideAvatarsEntity.addComponent(new AvatarModifierArea({ area: { box: new Vector3(16, 4, 11) }, modifiers: [AvatarModifiers.HIDE_AVATARS] }))
-hideAvatarsEntity.addComponent(new Transform({ position: new Vector3(8, 2, 10.5) }))
+hideAvatarsEntity.addComponent(
+  new AvatarModifierArea({
+    area: { box: new Vector3(16, 4, 11) },
+    modifiers: [AvatarModifiers.HIDE_AVATARS],
+  })
+)
+hideAvatarsEntity.addComponent(
+  new Transform({ position: new Vector3(8, 2, 10.5) })
+)
 engine.addEntity(hideAvatarsEntity)
 
 // Create to show Arissa avatar
 hideAvatarsEntity.addComponent(
   new utils.TriggerComponent(
-    new utils.TriggerBoxShape(new Vector3(16, 4, 11), Vector3.Zero()), 
-    null, null, null, null,
-    () => { arissa.getComponent(Transform).scale.setAll(1) },
-    () => { arissa.getComponent(Transform).scale.setAll(0) }
+    new utils.TriggerBoxShape(new Vector3(16, 4, 11), Vector3.Zero()),
+    {
+      onCameraEnter: () => {
+        arissa.getComponent(Transform).scale.setAll(1)
+      },
+      onCameraExit: () => {
+        arissa.getComponent(Transform).scale.setAll(0)
+      },
+    }
   )
 )
 
@@ -31,7 +49,7 @@ let currentPosition = new Vector3()
 
 class CheckPlayerIsMovingSystem implements ISystem {
   update() {
-    if(currentPosition.equals(Camera.instance.position)) {
+    if (currentPosition.equals(Camera.instance.position)) {
       arissa.playIdle()
     } else {
       currentPosition.copyFrom(Camera.instance.position)
